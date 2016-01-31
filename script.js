544deda8e5ed46e6d1f4c2c0221a543aa7206773
honorsprogramguy <jaquinny7615@bxcsm.org>
2016-01-31 15:02:29 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var userAnswer = prompt("Are you a new user?");
+    if (userAnswer == "yes"){
+        var userAnswer2 = prompt("What is your name?");
+            if (userAnswer2 == "Justin Rodriguez"){
+                var userAnswer3 = prompt("What is your password?");
+                    if (userAnswer3 == "Donuts"){
+                        var userAnswer4 = prompt("What is your age?")
+                            if (userAnswer4 == "18"){ 
+                                var userAnswer5 = prompt("What is your gender?");
+                                    if (userAnswer5 == "Male"){
+                                        var userAnswer6 = prompt("What school are you from?");
+                                            if (userAnswer6 == "Bronx Center for Science and Math"){
+                                                var userAnswer7 = prompt("What is your grade level?");
+                                                    if (userAnswer7 == "12"){
+                                                        var userAnswer8 = prompt("What is your GPA?");
+                                                            if (userAnswer8 == "3.9"){
+                                                                confirm("Yay!!! You have signed into MotivatUS!");
+                                                            }
+                                                    }
+                                            }
+                                    } 
+                            }
+                        
+                    }
+            }
+}
+    else{
+        confirm("Welcome back!!!")
+    } 
